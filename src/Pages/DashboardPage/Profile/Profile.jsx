dc3a7f96c75e1965303f72e68db7d218dbef91ac
sheikhtasmia25@gmail.com <sheikhtasmia25@gmail.com>
2026-04-24 02:45:27 +0600

    change done

--- src/Pages/DashboardPage/Profile/Profile.jsx
+++ src/Pages/DashboardPage/Profile/Profile.jsx
@@ -264,7 +264,7 @@
 
 import React, { useContext, useState, useEffect } from "react";
 import { FaUserEdit, FaSave, FaTimes, FaEnvelope, FaUser, FaCheckCircle, FaCamera } from "react-icons/fa";
-import { AuthContext } from "../../../providers/AuthProvider";
+import { AuthContext } from "../../../Providers/AuthProvider";
 
 const Profile = () => {
   const { user, loading, updateUserProfile } = useContext(AuthContext);
